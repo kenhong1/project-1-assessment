9d4b8eb9a9a74767bedf08e8959e2bf73a7eb75d
kenneth hong <kennethhong@kenneths-MacBook-Pro.local>
2019-02-12 15:59:26 -0800

    final version

---
+++ js/main.js
@@ -0,0 +1,91 @@
+console.log("hello world")
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Steps ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+//first create DOMCONTENTLOADED
+//append the buttons to the input box so output will register in the div. 
+//create functions that will add and subtract numbers 
+//call functions within the btns 
+
+var total; 
+var currentTotal; 
+var result = document.getElementById("numberOutputBox");
+var plus = document.getElementById("addBtn");
+var subtract = document.getElementById("minusBtn");
+var userEnterBox = document.getElementById("userNumber")
+
+
+
+
+//adding numbers function
+function addingNumbers(){
+    //binded user input to output 
+    var bindNumber = document.getElementById("userNumber").value;
+        document.getElementById("numberOutputBox").textContent = bindNumber;
+
+     total = parseInt(userNumber.value); 
+        currentTotal += total; 
+    init();
+
+console.log("you are going to add now")
+}
+
+
+//subtracting numbers function 
+function subtractingNumbers(){
+    //appended user input to output
+    var bindNumber = document.getElementById("userNumber").value;
+        document.getElementById("numberOutputBox").textContent = bindNumber;
+
+    total = parseInt(userNumber.value); 
+        currentTotal -= total; 
+    init();
+
+console.log("you are going to subtract now")
+}
+
+
+
+//addition button
+document.getElementById("addBtn").addEventListener("click",function(){
+console.log("adding completed")
+
+//call the adding function
+addingNumbers();
+})
+
+
+//subtraction button 
+document.getElementById("minusBtn").addEventListener("click",function(){
+console.log("subtracting completed")
+
+//call  the subtraction function
+subtractingNumbers();
+})
+
+//starts running current total 
+function start(){
+    currentTotal = 0;
+    init();
+}
+
+
+//DOM Content Loaded
+document.addEventListener("DOMContentLoaded", function(){
+    console.log("DOM is Loaded")
+    start(); 
+    init();
+})
+
+//if statement to color check if its positive or negative
+function init() {
+    if (currentTotal >= 0) {
+        result.style.color = "black";
+        result.textContent = currentTotal;
+    } else {
+        result.style.color = "red";
+        result.textContent = currentTotal;
+    }; 
+};
+
+
